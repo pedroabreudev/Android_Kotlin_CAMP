d366b9c94b0c0509abd48a62fb5a884ad46f813f
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2023-12-01 12:38:54 -0400

    Configura módulos do projeto.

--- app/src/main/java/com/pedroabreudev/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/pedroabreudev/marvelapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.pedroabreudev.marvelapp
+package com.pedroabreudev.marvelapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pedroabreudev.marvelapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
